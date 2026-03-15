4dfccadaa8bcb587c2da65126631991bafcca3cb
ahmadrojahakam1-design <ahmadrojahakam1@gmail.com>
2026-03-15 23:15:44 +0800

    Add files via upload

---
+++ LingkaranMain.java
@@ -0,0 +1,16 @@
+package pemlanModul1Bab3.OverloadingConstructor;
+
+public class LingkaranMain {
+    public static void main(String[] args) {
+        Lingkaran l = new Lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+
+        Lingkaran l2 = new Lingkaran(4, 10);
+        l2.displayMessage();
+        
+        Lingkaran Lstring = new Lingkaran("10", "5");
+        Lstring.displayMessage();
+    }
+}
+
---
+++ OverloadingMethod.java
@@ -0,0 +1,31 @@
+package pemlanModul1Bab3;
+import java.util.Scanner;
+
+public class OverloadingMethod {
+
+    public static void HitungLuas (int a, int b) {
+        int nilai = a*b;
+        System.out.println("Maka Hasil Luas: " + nilai);
+    }
+    
+    public static void HitungLuas (double value, double value2) {
+        double nilai = value * value2;
+        System.out.println("Maka Hasil Luas: " + nilai);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai integer 1: ");
+        int integer1 = in.nextInt();
+        System.out.print("Masukkan nilai integer 2: ");
+        int integer2 = in.nextInt();
+        HitungLuas (integer1, integer2);
+
+        System.out.print("Masukkan nilai double 1: ");
+        double double1 = in.nextDouble();
+        System.out.print("Masukkan nilai double 2: ");
+        double double2 = in.nextDouble();
+        HitungLuas (double1, double2);
+    }
+}
